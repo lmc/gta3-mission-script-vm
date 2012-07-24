631be23d2b47104dd3a977205e616565d2069575
Luke Mcildoon <luke@twofiftyfive.net>
2012-07-24 17:27:02 +1000

    more specs

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-# gem "rails"
+gem "rack"
 gem "rspec"
+
--- spec/vm_spec.rb
+++ spec/vm_spec.rb
@@ -5,8 +5,7 @@ describe Vm do
   describe "disassembly" do
 
     it "should disassemble opcode" do
-      opcode = vm_with_mem("04 00  02 0c 01  04 00").disassemble_opcode_at(0)
-      opcode.should == [[4, 0], [[2, [12, 1]], [4, [0]]]]
+      disassemble_opcode("04 00  02 0c 01  04 00").should == [[4, 0], [[2, [12, 1]], [4, [0]]]]
     end
 
   end  
@@ -15,7 +14,31 @@ describe Vm do
   describe "data types" do
     
     describe "disassemble_opcode_arg_at" do
-      
+      it "should disassemble int32s" do
+        disassemble_arg("01 20 86 00 00").should == [1, [32, 134, 0, 0]]
+      end
+      it "should disassemble pg_ifs" do
+        disassemble_arg("02 20 86").should == [2, [32, 134]]
+      end
+      it "should disassemble int8s" do
+        disassemble_arg("04 fe").should == [4, [254]]
+      end
+      it "should disassemble int16s" do
+        disassemble_arg("05 20 40").should == [5, [32, 64]]
+      end
+      it "should disassemble float32s" do
+        disassemble_arg("06 fe b5 70 00").should == [6, [254, 181, 112, 0]]        
+      end
+      it "should disassemble immediate 8-byte strings" do
+        disassemble_arg("09 90 90 90 90 91 91 91 91").should == [9, [144, 144, 144, 144, 145, 145, 145, 145]]
+      end
+      it "should disassemble variable-length strings" do
+        disassemble_arg("0e 02 01 02").should == [14, [2, 1, 2]]
+        disassemble_arg("0e 04 01 02 03 04").should == [14, [4, 1, 2, 3, 4]]
+      end
+      it "should disassemble type-less immediate 8-byte strings" do
+        disassemble_arg("91 92 93 94 95 96 97 98").should == [145, [146, 147, 148, 149, 150, 151, 152]]
+      end
     end
 
     describe "arg_to_native" do
@@ -26,7 +49,11 @@ describe Vm do
 
 
   describe "variable arguments" do
-    
+    it "should disassemble opcodes with a variable number of args" do
+      disassemble_opcode("4f 00 01 01 02 03 04 04 ff 02 ee ee 00").should == [
+        [79, 0], [[1, [1, 2, 3, 4]], [4, [255]], [2, [238, 238]], [0]]
+      ]
+    end
   end
 
 
@@ -34,6 +61,14 @@ describe Vm do
     
   end
 
+end
+
+def disassemble_opcode(memory)
+  vm_with_mem(memory).disassemble_opcode_at(0)
+end
+
+def disassemble_arg(memory)
+  vm_with_mem(memory).disassemble_opcode_arg_at(0)
 end
 
 def vm_with_mem(memory)
